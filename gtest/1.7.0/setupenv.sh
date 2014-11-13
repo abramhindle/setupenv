c5e6f3d8a3972fa1da97d3072295d4c9f3744066
Malte Nuhn <nuhn@cs.rwth-aachen.de>
2014-11-13 15:21:56 +0100

    fix ftest

--- gtest/1.7.0/setupenv.sh
+++ gtest/1.7.0/setupenv.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )"
 
-export LD_LIBRARY_PATH=${BASEDIR}/src/lib:${LD_LIBRARY_PATH}
 export LDFLAGS="${LDFLAGS} -L${BASEDIR}/src/lib"
 export CPPFLAGS="${CPPFLAGS} -isystem${BASEDIR}/src/include"
+export LD_RUN_PATH=${BASEDIR}/bin/lib:${LD_RUN_PATH}
+export LD_LIBRARY_PATH=${BASEDIR}/bin/lib:${LD_LIBRARY_PATH}
