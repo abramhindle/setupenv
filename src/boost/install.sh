a7fd77eb56b82af811489408f066df935e06152e
Malte Nuhn <nuhn@cs.rwth-aachen.de>
2014-11-19 00:53:43 +0100

    add boost

---
+++ src/boost/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+WHAT=boost
+VERSION=1.57.0
+TARGETDIR=`realpath ../../build`/${WHAT}-${VERSION}
+
+if [ -d "${TARGETDIR}" ]; then
+  echo >&2 "${TARGETDIR} already exists"
+  exit 1
+fi
+
+wget http://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.gz
+tar xvfz boost_1_57_0.tar.gz
+mv boost_1_57_0 ${VERSION}
+cd ${VERSION}
+
+./bootstrap.sh --with-libraries=filesystem,program_options,system,atomic,log,regex,thread,timer,signals,iostreams,chrono --prefix=${TARGETDIR}
+./b2 -j4
+./b2 install
