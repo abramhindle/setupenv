181bbcb1af2c441d3a9d2be27c19bde205838375
Malte Nuhn <painted.now@gmail.com>
2014-12-08 02:31:49 +0100

    newest

---
+++ src/glog/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+WHAT=glog
+VERSION=0.3.3
+TARGETDIR=`realpath ../../build`/${WHAT}-${VERSION}
+
+if [ -d "${TARGETDIR}" ]; then
+  echo >&2 "${TARGETDIR} already exists"
+  exit 1
+fi
+
+wget https://google-glog.googlecode.com/files/glog-${VERSION}.tar.gz
+
+tar xf glog-${VERSION}.tar.gz
+mv glog-${VERSION} ${VERSION}
+cd ${VERSION}
+./configure --prefix=${TARGETDIR}
+make
+make install
