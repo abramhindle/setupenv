1d581ee6bd1f1ee527ab0901117b763580a57b83
Malte Nuhn <painted.now@gmail.com>
2015-01-04 14:59:45 -0500

    add clang

---
+++ src/clang/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+WHAT=clang
+VERSION=3.5.0
+TARGETDIR=`realpath ../../build`/${WHAT}-${VERSION}
+
+if [ -d "${TARGETDIR}" ]; then
+  echo >&2 "${TARGETDIR} already exists"
+  exit 1
+fi
+
+wget http://llvm.org/releases/3.5.0/clang+llvm-3.5.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+tar xvf clang+llvm-3.5.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+mv clang+llvm-3.5.0-x86_64-linux-gnu ${TARGETDIR}
