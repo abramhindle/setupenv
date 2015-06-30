5e2389d074602ba7016093b45ba8cc360eb4e32d
Xiaoyu Wang <xuanhuangyiqi@126.com>
2015-06-30 12:43:03 +0000

    -N

--- src/root/install.sh
+++ src/root/install.sh
@@ -4,6 +4,6 @@ WHAT=root
 VERSION=6.03.04
 TARGETDIR=`realpath ../../build`/${WHAT}-${VERSION}
 
-wget http://root.cern.ch/download/root_v${VERSION}.source.tar.gz
+wget -N http://root.cern.ch/download/root_v${VERSION}.source.tar.gz
 
 tar xvfz root_v${VERSION}.source.tar.gz
